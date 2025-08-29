06d154e44f9fdf91cba3a8d95f8d5b5c9b8082e6
kim taedong <rlaxoehd4234@naver.com>
2025-08-29 14:05:51 +0900

    'initial'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        TEST
-      </h1>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/posts">Posts</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/posts" element={<h1>Post List</h1>} />
+        <Route path="/posts/:id" element={<h1>Post Detail</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
